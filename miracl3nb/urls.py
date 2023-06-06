905080a36aef2d9d96651a5dd7cc308510eed6ff
Miracl3nb <109076069+Miracl3nb@users.noreply.github.com>
2023-06-06 16:25:11 -0300

    Cambios

--- miracl3nb/urls.py
+++ miracl3nb/urls.py
@@ -25,7 +25,7 @@ from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path("", views.home),
     path('portfolio/', include('portfolio.urls')),
     path('blog/', include('blog.urls')),
 
